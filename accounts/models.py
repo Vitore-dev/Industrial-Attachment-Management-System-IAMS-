d04d1879f53b6d70187e118ae746e90571c5b630
Vitore-dev <uakamburavitore@gmail.com>
2026-03-19 23:47:42 +0200

    Add accounts model and update settings.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('organization', 'Organization'),
+        ('coordinator', 'Coordinator'),
+        ('university_supervisor', 'University Supervisor'),
+        ('industrial_supervisor', 'Industrial Supervisor'),
+    ]
+    
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
